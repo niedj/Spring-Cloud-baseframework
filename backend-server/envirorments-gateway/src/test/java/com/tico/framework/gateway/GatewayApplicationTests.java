30c6d1895ebe57d490fe5d3edb0d7723062176d5
niedj <pop_ret@126.com>
2020-05-17 01:03:24 +0800

    网关服务

---
+++ backend-server/envirorments-gateway/src/test/java/com/tico/framework/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tico.framework.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
