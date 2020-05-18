62b51ee495db100972f1265be6f9135015162090
niedj <pop_ret@126.com>
2020-05-19 01:47:19 +0800

    修改配置文件

---
+++ backend-server/envirorments/envirorments-configserver/src/main/java/com/sc/shutdown/ShutDownAfterProcess.java
@@ -0,0 +1,31 @@
+/**
+ * Project Name:envirorments-configserver
+ * File Name:ShutDownAfterProcess.java
+ * Package Name:com.sc.shutdown
+ * Date:2020-05-19 01:01:52
+ * Copyright (c) 2020, CCS DIG All Rights Reserved.
+ *
+ */
+
+package com.sc.shutdown;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sc.framework.hoxton.basecomponent.shutdown.grace.core.ShutdownJob;
+
+public class ShutDownAfterProcess implements ShutdownJob {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /*
+     * (non-Javadoc) curl -X POST http://127.0.0.1:18796/actuator/shutdown
+     * @see com.gxlu.shutdown.grace.core.ShutdownJob#executeShutdownJob()
+     */
+    @Override
+    public void executeShutdownJob() {
+        // TODO Auto-generated method stub
+        this.log.info("Config server is shutdown !!!");
+    }
+
+}
--- backend-server/envirorments/envirorments-gateway/src/main/java/com/sc/gateway/GatewayApplication.java
+++ backend-server/envirorments/envirorments-gateway/src/main/java/com/sc/gateway/GatewayApplication.java
@@ -1,15 +1,25 @@
-package com.sc.framework.hoxton.gateway;
+package com.sc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import com.sc.framework.hoxton.basecomponent.shutdown.grace.core.ShutdownJob;
+import com.sc.shutdown.ShutDownAfterProcess;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public ShutdownJob shutdownJob() {
+        return new ShutDownAfterProcess();
+    }
 }
